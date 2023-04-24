bfe27855cc4046c12782cbd5cb44a8e960d6c944
JEGATHEESWARAN T <727721eucs048@skcet.ac.in>
2023-04-24 16:30:04 +0530

    avatar_updated

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -1,19 +1,66 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import { blue } from '@mui/material/colors';
+import "./css/avatar.css";
+import { PopupMenu } from "react-simple-widgets";
 
-export default function FallbackAvatars() {
+export default function Avatar() {
   return (
-    <Stack direction="row" spacing={2}>
-      <Avatar
-        sx={{ bgcolor: blue[900] }}
-        alt="Remy Sharp"
-        
-      >
-        J
-      </Avatar>
-    
-    </Stack>
+    <div id="app">
+      <div className="text-end">
+        <PopupMenu>
+          {/* <button className="btn btn-primary"> */}
+            <img className="avatar_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpThzhMB0Esd2PRg2vHdy8YdMQqD6C0daU8w&usqp=CAU" />
+          {/* // </button> */}
+
+          <div className="card text-start">
+            <div className="card-body px-4 py-4">
+              <div id="circle-avatar" className="text-center mx-auto mb-4">
+                <span>K</span>
+              </div>
+
+              <h5 className="text-center mb-0">John Doe</h5>
+              <p className="text-center mb-2">jd@gmail.com</p>
+
+              <hr />
+
+              <p
+                className="mb-0"
+                style={{ color: "#bebebe", fontWeight: "bold", fontSize: 12 }}
+              >
+                ROLES
+              </p>
+              <p style={{ fontSize: 12 }}>
+                {["Submitter", "Project manager", "Change control board"].join(
+                  ", "
+                )}
+              </p>
+
+              <hr className="mb-0" style={{ margin: "0 -24px 0" }} />
+
+              <div
+                className="list-group list-group-flush"
+                style={{ margin: "0 -24px 0" }}
+              >
+                <button className="list-group-item list-group-item-action px-4">
+                  <small>Change Requests</small>
+                </button>
+                <button className="list-group-item list-group-item-action px-4">
+                  <small>Pending Requests</small>
+                </button>
+                <button className="list-group-item list-group-item-action px-4">
+                  <small>Other Requests</small>
+                </button>
+              </div>
+
+              <hr style={{ margin: "0 -24px 24px" }} />
+
+              <div className="d-grid">
+                <button className="btn btn-secondary">
+                  <small>Logout</small>
+                </button>
+              </div>
+            </div>
+          </div>
+        </PopupMenu>
+      </div>
+    </div>
   );
 }
