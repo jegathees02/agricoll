022cb6ad4eb2e47191086dd9e1c3a5a23d0835c6
sanjeevi7777 <104307183+sanjeevi7777@users.noreply.github.com>
2023-04-25 14:07:02 +0530

    input_changes

--- src/App.js
+++ src/App.js
@@ -8,21 +8,24 @@ import Tomato from "./components/tomato";
 import Dashboard from "./components/dashboard";
 import Brinjal from "./components/brinjal";
 import Weather from "./components/weather";
+import Input from "./components/input";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      {/* <Router>
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/onion" element={<Onion />} />
           <Route path="/tomato" element={<Tomato />} />
           <Route path="/Cropplan" element={<CropPlan/>} />
           <Route path="/Brinjal" element={<Brinjal />} />
+          <Route path="/Input" element={<Input />} />
         </Routes>
-      </Router>
+      </Router> */}
+      <Input/>
     </div>
   );
 }
---
+++ src/components/Input2.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+function Input2() {
+    return (<div>
+        <h1>Input2</h1>
+    </div>);
+}
+
+export default Input2;
