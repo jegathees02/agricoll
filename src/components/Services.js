feb96e7e0a45ea3ea2882a6ca69aa014cb4ba729
sanjeevi7777 <104307183+sanjeevi7777@users.noreply.github.com>
2023-04-26 10:48:59 +0530

    changes

---
+++ src/components/Services.js
@@ -0,0 +1,27 @@
+import Navbar from "./navbar";
+import './css/Services.css'
+function Services() {
+    return ( 
+        <>
+        <Navbar/>
+        <div className="container">
+            <div className="img1">
+                <h1>nvwhfouq</h1>
+            </div>
+            <div className="cont1">
+                <h1>
+                    SERVICES
+                </h1>
+                <ul>
+                    <ol>Soil Testing</ol>
+                    <ol>Conversion of soil</ol>
+                    <ol>Guiding farmers</ol>
+                    <ol>Making environment sustainable</ol>
+                </ul>
+            </div>
+        </div>
+        </>
+     );
+}
+
+export default Services;
